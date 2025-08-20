68c06978a46dc50f655fd49deb2cd983a2c0065f
Rupesh Pal <palrupesh292@gmail.com>
2025-08-20 11:43:03 +0545

    add section class subject

--- app/Models/Teacher/Teacher.php
+++ app/Models/Teacher/Teacher.php
@@ -4,6 +4,7 @@ namespace App\Models\Teacher;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Teacher\TeacherAssignment;
 
 class Teacher extends Model
 {
@@ -19,13 +20,15 @@ class Teacher extends Model
         'status' => 'boolean',
     ];
 
-   protected $fillable = [
-    'teacher_code', 'first_name', 'last_name', 'email', 'primary_contact',
-    'gender', 'blood_group', 'date_of_birth', 'date_of_joining', 'marital_status',
-    'qualification', 'work_experience', 'father_name', 'mother_name',
-    'house', 'mother_tongue', 'status'
-];
+    protected $fillable = [
+        'teacher_code', 'first_name', 'last_name', 'email', 'primary_contact',
+        'gender', 'blood_group', 'date_of_birth', 'date_of_joining', 'marital_status',
+        'qualification', 'work_experience', 'father_name', 'mother_name',
+        'house', 'mother_tongue', 'status'
+    ];
 
-
-
+    public function assignments()
+    {
+        return $this->hasMany(TeacherAssignment::class, 'teacher_id', 'id');
+    }
 }
